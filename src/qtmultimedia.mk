39278b1ed3d3c798d46841d5dd67a064d054bda5
Nathanael Landais <nathanael@lenewt.com>
2025-06-03 15:11:55 +0200

    Fix qtmultimedia missing from QML

--- src/qtmultimedia.mk
+++ src/qtmultimedia.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := 4a76bfc2567de7a72ae22156e4d49e186d889848
 $(PKG)_SUBDIR    = $(subst qtbase,qtmultimedia,$(qtbase_SUBDIR))
 $(PKG)_FILE      = $(subst qtbase,qtmultimedia,$(qtbase_FILE))
 $(PKG)_URL       = $(subst qtbase,qtmultimedia,$(qtbase_URL))
-$(PKG)_DEPS     := gcc qtbase
+$(PKG)_DEPS     := gcc qtbase qtdeclarative
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
